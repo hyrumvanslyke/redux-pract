93769ee4b076b890a9efe8daf2ab61d1bc7b90e4
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-10-03 15:54:20 -0400

    STart of redux tool kit

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,38 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
+export const INCREMENT = 'increment'
+
+const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
+  if (action.type === INCREMENT) {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter
+    };
+  }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.value,
+      showCounter: state.showCounter
     };
   }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter
     };
   }
+  if(action.type === 'toggle'){
+    return{
+        showCounter: !state.showCounter,
+        counter: state.counter
+
+    }
+  }
+
   return state;
 };
 
 const store = createStore(counterReducer);
 
 
-
-store.dispatch({ type: "increment" });
-store.dispatch({ type: "decrement" });
-
-export default store
+export default store;
